d10660f3a3d440c22effe44295cc8671a63ff34c
Artsiom Musin <artyom.musin@gmail.com>
2020-07-20 17:27:05 +0300

    Add user model and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < Sequel::Model
+  NAME_FORMAT = %r{\A\w+\z}
+
+  def validate
+    super
+
+    validates_presence :name, message: I18n.t(:blank, scope: 'model.errors.user.name')
+    validates_presence :email, message: I18n.t(:blank, scope: 'model.errors.user.email')
+    validates_format NAME_FORMAT, :name, message: I18n.t(:invalid, scope: 'model.errors.user.name')
+  end
 end
